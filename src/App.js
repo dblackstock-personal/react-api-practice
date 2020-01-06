aa7c0fee3a54f9ace9667862387850d0cdd310e4
dblackstock-personal <davey.blackstock@gmail.com>
2020-01-06 10:20:48 +0000

    working example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import { Users } from './components/Users.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+state = {
+  users: [],
+  loading: false
+}
+
+// this runs when the page is loaded
+// it's a great place to grab data from an api
+async componentDidMount() {
+
+  this.setState({   //we don't have the data yet so let's set the spinner on
+    loading:true
+  });
+
+  const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+
+  this.setState({
+    users: response.data,
+    loading:false       //data has been received, let's turn the spinner off!
+  });
+}
+
+  render(){
+    // variables go inside the render but before the return
+    const allUsers = this.state.users.map( (object,index) => {
+      //map functions need a return statement
+      return (
+      <div key={object.id}>
+      <h1>My name is {object.name}, I'm from {object.address.city}</h1>
+      <h3>hit me up at {object.email}</h3>
+      </div>)
+      
+      
+    });
+
+    return(
+      <div>
+        <Users data={allUsers} loading={this.state.loading}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Users.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Spinner } from './Spinner.js';
+
+export const Users = (props) => {
+
+            if(props.loading){
+                return <Spinner/>
+            } else {
+                return (
+                    <div>
+                        {props.data}
+                    </div>
+                )
+            }
+
+}
